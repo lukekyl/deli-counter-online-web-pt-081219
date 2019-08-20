1a2640f9b4e5f55d9cbd34e1efe306ba7d8fc0ef
Kyle Luke <th.oldschool@gmail.com>
2019-08-20 03:23:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  line_array = katz_deli
+  if line_array.length == 0
+    puts "The line is currently empty."
+  else 
+    counter = 1
+    line_list = ""
+    line_array.each{|name|
+      if name == katz_deli.first
+        line_list << "The line is currently: #{counter}. #{name}"
+        counter += 1
+      else
+        line_list << " #{counter}. #{name}"
+        counter += 1
+      end
+      }
+      puts line_list
+  end
+end
+
+def take_a_number(katz_deli, additions)
+  line_array = katz_deli
+  counter = katz_deli.length
+  additions.each{|name|
+    counter += 1
+    line_array << name
+    p "Welcome #{name}. You are number #{counter} in line."
+  }
+  return line_array
+end
