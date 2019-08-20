47f4b5c0a4f2c8fa1849d6aa5eb092611436ab8c
Kyle Luke <th.oldschool@gmail.com>
2019-08-20 03:31:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,11 +29,12 @@ def take_a_number(katz_deli, additions)
     line_message = "Welcome, #{additions}. You are number #{counter} in line."
     puts line_message
   else
-    additions.each{|name|
-      counter += 1
-      line_array << name
-      puts "Welcome, #{name}. You are number #{counter} in line."
-    }
+    
+    #additions.each{|name|
+    # counter += 1
+    #  line_array << name
+    #  puts "Welcome, #{name}. You are number #{counter} in line."
+    #}
   end
   return line_array
 end
